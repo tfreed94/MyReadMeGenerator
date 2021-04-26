e9a83827ad0f8610fc88e64d5b1e1e0c9d3d4b3c
Troy Freed <tfreed94@gmail.com>
2021-04-26 05:13:55 -0400

    updated index.js

--- index.js
+++ index.js
@@ -102,6 +102,6 @@ inquirer.prompt([
     .then((userInput) => {
         const createREADME = generatedREADME(userInput);
         // Let user know if there is an error or if the README.md was successfully created
-        fs.writeFile("SampleREADME.md", createREADME, (err) => err ? console.log(err) : console.log("README has be successfully created"));
+        fs.writeFile("README.md", createREADME, (err) => err ? console.log(err) : console.log("README has be successfully created"));
     });
 
